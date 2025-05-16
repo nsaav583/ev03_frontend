eac04a0b2ecf29e18be8f7917873d0c023ffd4d7
Atila_Huno <felipe.juancin.castillo@hotmail.com>
2025-05-16 17:37:03 -0400

    inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+  return (
+    <>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+<Button>
+  Guardar
+</Button>
+    </>
+  )
+}
+
+export default App
